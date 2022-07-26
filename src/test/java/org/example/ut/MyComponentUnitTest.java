0434237321b368aba3bb09cd769f5aa38965d374
salomonfx <99074676+salomonfx@users.noreply.github.com>
2022-07-26 16:28:10 +0300

    Initial commit

---
+++ src/test/java/org/example/ut/MyComponentUnitTest.java
@@ -0,0 +1,17 @@
+package ut.org.example;
+
+import org.junit.Test;
+import org.example.MyPluginComponent;
+import org.example.MyPluginComponentImpl;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyComponentUnitTest
+{
+    @Test
+    public void testMyName()
+    {
+        MyPluginComponent component = new MyPluginComponentImpl(null);
+        assertEquals("names do not match!", "myComponent",component.getName());
+    }
+}
